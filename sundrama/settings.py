19dab25b564d27ee8f1d8ad3517f4f6517462577
macscream <sinansungur@live.com>
2025-05-17 01:26:01 +0100

    Update settings.py

--- sundrama/settings.py
+++ sundrama/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sundrama-panel.onrender.com', 'sundrama.co.uk']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
